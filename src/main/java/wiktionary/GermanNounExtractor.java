203070f2848d5d812260ca4276554b053259c8e8
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2018-04-09 22:00:24 +0200

    german nouns extractor

--- src/main/java/wiktionary/GermanNounExtractor.java
+++ src/main/java/wiktionary/GermanNounExtractor.java
@@ -1,18 +1,58 @@
 package wiktionary;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GermanNounExtractor {
     public static void main(String[] args) throws IOException {
         DumpReader reader = new DumpReader("data/dewiktionary-latest-pages-articles.xml.bz2");
 
+        Pattern genusPattern = Pattern.compile("\\|genus=(\\w+)");
+        //Pattern singularPattern = Pattern.compile("\\|nominativ singular=(\\w+)");
+
+        PrintWriter pw = new PrintWriter("results.txt");
+
         while (reader.hasNext()) {
             Page page = reader.next();
-            System.out.println(page.getTitle());
-            System.out.println(page.getContent());
-            System.out.println();
+
+            String content = page.getContent().toLowerCase();
+            if (!content.contains("{{deutsch substantiv")) {
+                continue;
+            }
+
+            Matcher genusMatcher = genusPattern.matcher(content);
+            if (!genusMatcher.find()) {
+                continue;
+            }
+
+            String genus = genusMatcher.group(1);
+            String artikel = toArtikel(genus);
+
+            if (artikel == null) {
+                continue;
+            }
+
+            System.out.println(artikel + " " + page.getTitle());
+            pw.println(artikel + "\t" + page.getTitle());
         }
 
+        pw.flush();
+        pw.close();
+
         reader.close();
+    }
+
+    private static String toArtikel(String genus) {
+        if ("m".equals(genus)) {
+            return "der";
+        } else if ("f".equals(genus)) {
+            return "die";
+        } else if ("n".equals(genus)) {
+            return "das";
+        }
+
+        return null;
     }
 }
